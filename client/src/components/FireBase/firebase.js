520ce51a74333277a157f0b1cfe27c7feff98731
Spandan Patil <spandanmpatil@gmail.com>
2025-10-22 21:48:43 -0700

    Clear Firebase configuration values
    Removed sensitive Firebase configuration details.

--- client/src/components/FireBase/firebase.js
+++ client/src/components/FireBase/firebase.js
@@ -2,12 +2,12 @@ import firebase from "firebase/app";
 import "firebase/auth";
 
 const app = firebase.initializeApp({
-  apiKey: "AIzaSyArWBo1luvgxloKUJmx5sXtaVBopYbNxMI",
-  authDomain: "flip-dev-5d718.firebaseapp.com",
-  projectId: "flip-dev-5d718",
-  storageBucket: "flip-dev-5d718.appspot.com",
-  messagingSenderId: "674781734691",
-  appId: "1:674781734691:web:38785d653c7ed91f234e96",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
 });
 
 export const auth = app.auth();
